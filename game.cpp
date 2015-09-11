2d55e7d7684930ede9720358ae1df454627006ce
zivlakmilos <zivlakmilos@gmail.com>
2015-09-12 00:15:08 +0200

    Tamno smedja pozadina - tamnije

--- game.cpp
+++ game.cpp
@@ -40,7 +40,7 @@ void Game::init(void)
 
     // Specific the clear color
     //glClearColor(0, 0, 0, 1);   // Black color
-    glClearColor(0.09, 0, 0, 1);
+    glClearColor(0.05, 0, 0, 1);
 
     // What portion of the screen we will display
     glViewport(0, 0, this->width, this->height);
